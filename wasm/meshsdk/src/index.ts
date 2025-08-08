dc29469c6b1878407730537181a493b28c0d2c47
zzz <imzqqq@hotmail.com>
2025-08-09 02:48:32 +0800

    Add node & web environments for meshsdk.

--- wasm/meshsdk/src/index.ts
+++ wasm/meshsdk/src/index.ts
@@ -3,5 +3,5 @@ import createMeshSDK from './MRJavaScript.js';
 export { createMeshSDK };
 export const create = createMeshSDK;
 
-export type MRMesh = Awaited<ReturnType<typeof createMeshSDK>>;
-export const MRMesh = { create: createMeshSDK };
+export type MeshSDK = Awaited<ReturnType<typeof createMeshSDK>>;
+export const MeshSDK = { init: createMeshSDK };
